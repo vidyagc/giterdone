69f91e0fd22b4717c92ee348b8807249e82f6fa5
Vidya Neni <vidyagc16@gmail.com>
2017-11-17 16:19:17 -0600

    ng-show hide update row, added update row with radio buttons, and calculated priorities in controller

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -4,14 +4,47 @@
         this.tasksIncomplete = Task.tasksIncomplete;
 
         this.addTask = function() {
+
           if (this.newTask.priority==undefined)
           {
-              this.newTask.priority="2";
+            this.newTask.priority="2";
           }
             this.newTask.status = 'incomplete';
             this.newTask.date = Math.round(Date.now() / 1000);
             this.tasksIncomplete.$add(this.newTask);
             this.newTask = {}
+        }
+
+
+        this.getPriority = function(p) {
+
+          var priorityArray = ["High", "Medium", "Low"];
+          pArrayVals = [["High", 1], ["Medium", 2], ["Low",3]]
+          var newPriorities = [];
+          var index=parseInt(p)-1;
+
+          newPriorities.push(priorityArray[index]);
+          priorityArray.splice(index, 1);
+
+          newPriorities.push(priorityArray[0]);
+          newPriorities.push(priorityArray[1]);
+
+          return newPriorities;
+          }
+
+        this.pVal = function(priority) {
+          var val;
+          switch (priority) {
+            case 'High':
+              val = 1;
+              break;
+            case 'Medium':
+              val = 2;
+              break;
+            default:
+              val = 3;
+          }
+          return val;
         }
 
         this.completeTask = function(taskid) {
@@ -22,12 +55,30 @@
             });
           }
 
-            this.updateTask = function(taskid, blah) {
+            this.updateTask = function(taskid, newTitle, newPriority) {
+
+              var newP;
+              if (newPriority!=undefined) {
+                newP = newPriority;
+                  //console.log(newPriority);
+              }
+              else {
+                newP = taskid.title;
+              }
+
               firebase.database().ref('tasks/' + taskid.$id).set({
                   date: taskid.date,
-                  title: blah,
+                  title: newTitle,
+                  priority: newP,
                   status: taskid.status
+                  // if (newPriority!=undefined) {
+                  //   priority: newPriority
+                  // }
+                  // else {
+                  //   priority: taskid.priority
+                  // }
               });
+
         }
 
     }
